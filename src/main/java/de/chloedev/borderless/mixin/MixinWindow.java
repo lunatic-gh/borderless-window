90b475bc204cf02c55e4c2615ec743b663187ae9
ChloeCDN <chloe@chloedev.de>
2023-04-11 05:19:29 +0200

    Initial commit

---
+++ src/main/java/de/chloedev/borderless/mixin/MixinWindow.java
@@ -0,0 +1,19 @@
+package de.chloedev.borderless.mixin;
+
+import net.minecraft.client.util.Window;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Window.class)
+public class MixinWindow {
+
+    @Redirect(method = "updateWindowRegion", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwSetWindowMonitor(JJIIIII)V"))
+    public void setBorderless(long window, long monitor, int xpos, int ypos, int width, int height, int refreshRate) {
+        if (monitor != 0L) {
+            GLFW.glfwSetWindowSizeLimits(window, 0, 0, width, height);
+        }
+        GLFW.glfwSetWindowMonitor(window, 0L, xpos, ypos, width, height, refreshRate);
+    }
+}
